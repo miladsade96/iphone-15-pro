1aeed7ca1c20692a005cdcd6704341ca4cf0a216
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2024-05-17 13:35:29 +0330

    Adding hero component to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
 
 export default function App() {
   return (
     <main className="bg-black">
       <Navbar />
+      <Hero />
     </main>
   );
 }
