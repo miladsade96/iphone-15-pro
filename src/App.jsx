99bde868e252e9daea5f590dbb8472c450bd289c
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2024-05-18 12:53:00 +0330

    Implementing Highlights component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
+import Highlights from "./components/Highlights.jsx";
 
 export default function App() {
   return (
     <main className="bg-black">
       <Navbar />
       <Hero />
+      <Highlights />
     </main>
   );
 }
