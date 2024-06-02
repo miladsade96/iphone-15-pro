fe85c8f91f429400a31b8b364989d71b29d7e8f6
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2024-06-02 13:47:28 +0330

    Implementing initial markup

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,3 +1,19 @@
+import ModelView from "./ModelView.jsx";
 export default function Model() {
   return <h1>Model</h1>;
+  return (
+    <section className="common-padding">
+      <div className="screen-max-width">
+        <h1 id="heading" className="section-heading">
+          Take a closer look
+        </h1>
+
+        <div className="flex flex-col items-center mt-5">
+          <div className="w-full h-[75dvh] md:h-[90dvh] overflow-hidden relative">
+            <ModelView />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
